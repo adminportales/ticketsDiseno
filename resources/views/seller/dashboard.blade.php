79778f4d80222d7003f9a1947872ec2f919f827b
Antonio Tomas <adminportales@promolife.com.mx>
2021-11-17 10:20:10 -0600

    Correccion el dashboard del asistente

--- resources/views/seller/dashboard.blade.php
+++ resources/views/seller/dashboard.blade.php
@@ -80,7 +80,7 @@
                                             <td>{{ $ticket->designer_name }}</td>
                                             <td class="text-center"> {{ $ticket->priorityTicket->priority }} </td>
                                             <td> {{ $ticket->latestTicketInformation->created_at->diffForHumans() }}</td>
-                                            <td><a href="{{ route('designer.show', ['ticket' => $ticket->id]) }}"
+                                            <td><a href="{{ route('tickets.show', ['ticket' => $ticket->id]) }}"
                                                     class="boton">Ver
                                                     ticket</a></td>
                                         </tr>
