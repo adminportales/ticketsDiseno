b24393352322d08ba5a1f88878a2e0ca48b2e1df
Antonio Tomas <adminportales@promolife.com.mx>
2022-01-04 08:57:13 -0600

    Correccion en el sonido OTRA VEZ

--- resources/js/components/Notify.vue
+++ resources/js/components/Notify.vue
@@ -3,7 +3,6 @@
     <audio id="audio" controls>
       <source type="audio/wav" src="/assets/audio/notify.mp3" />
     </audio>
-    <button id="botonClickeable">Click</button>
   </div>
 </template>
 
@@ -13,66 +12,62 @@ export default {
   props: ["user"],
   mounted() {
     const audio = document.querySelector("#audio");
-    const botonClickeable = document.querySelector("#botonClickeable");
-    setTimeout(() => {
-      botonClickeable.click();
-    }, 1000);
     window.Echo.channel("delivery").listen("TicketDeliverySendEvent", (e) => {
-      //   console.log(e);
+      console.log(e);
       if (this.user == e.receptor) {
-        audio.play();
         toastr.success(
           `Entrego: ${e.emisor}<br/>Ticket: ${e.ticket}`,
           "Entrega de Archivos"
         );
+        audio.play();
       }
     });
     window.Echo.channel("notification").listen("MessageSendEvent", (e) => {
-    //   console.log(e);
+      console.log(e);
       if (this.user == e.receptor) {
-        audio.play();
         toastr.info(`${e.emisor}: ${e.message}`, "Mensaje");
+        audio.play();
       }
     });
     window.Echo.channel("status").listen("ChangeStatusSendEvent", (e) => {
-      //   console.log(e);
+      console.log(e);
       if (this.user == e.receptor) {
-        audio.play();
         toastr.warning(
           `Ticket: ${e.ticket}<br/> Estado: ${e.status}`,
           "Cambio de Estado"
         );
+        audio.play();
       }
     });
 
     window.Echo.channel("creado").listen("TicketCreateSendEvent", (e) => {
-      //   console.log(e);
+      console.log(e);
       if (this.user == e.receptor) {
-        audio.play();
         toastr.warning(
           `Creador: ${e.emisor}<br/>Ticket: ${e.ticket} `,
           "Creacion de Ticket"
         );
+        audio.play();
       }
     });
     window.Echo.channel("priority").listen("ChangePrioritySendEvent", (e) => {
-      //   console.log(e);
+      console.log(e);
       if (this.user == e.receptor) {
-        audio.play();
         toastr.error(
           `Creador: ${e.emisor}<br/>Prioridad: ${e.prioridad}<br/>Ticket: ${e.ticket} `,
           "Cambio de prioridad"
         );
+        audio.play();
       }
     });
     window.Echo.channel("change").listen("ChangeTicketSendEvent", (e) => {
-      //   console.log(e);
+      console.log(e);
       if (this.user == e.receptor) {
-        audio.play();
         toastr.error(
           `Modificado por: ${e.emisor}<br/>Ticket: ${e.ticket} `,
           "Cambio de Informacion"
         );
+        audio.play();
       }
     });
   },
@@ -81,14 +76,28 @@ export default {
       message: "",
     };
   },
+  methods: {
+    notify() {
+      if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+      } else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification("Hi there!");
+      } else if (Notification.permission !== "denied") {
+        Notification.requestPermission(function (permission) {
+          // If the user accepts, let's create a notification
+          if (permission === "granted") {
+            var notification = new Notification("Hi there!");
+          }
+        });
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
 #audio {
-  display: none;
-}
-#botonClickeable {
   display: none;
 }
 </style>
