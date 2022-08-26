6ce4e7ebae97408b2cf1eb920c72f1f6289073ae
Antonio Tomas <antoniotd87@gmail.com>
2022-08-26 13:01:11 -0500

    JS Modificado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,7 @@
     </div>
     <script src="{{ asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app1.js') }}"></script>
     <script src="{{ asset('assets/vendors/jquery/jquery.min.js') }}"></script>
     @yield('scripts')
     <script src="{{ asset('assets\vendors\toastify\toastify.js') }}"></script>
