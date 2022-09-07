d9774e779a0d5e27cea0129430e65b5001ffefb1
Antonio Tomas <antoniotd87@gmail.com>
2022-09-07 09:23:19 -0500

    Fix en la vista de tikets

--- resources/views/assistent/dashboard.blade.php
+++ resources/views/assistent/dashboard.blade.php
@@ -78,7 +78,7 @@
                                                 @if ($latestTicketInformation)
                                                     @if ($latestTicketInformation->techniqueTicket)
                                                         <strong>Tecnica:</strong>
-                                                        {{ $$latestTicketInformation->techniqueTicket->name }}<br>
+                                                        {{ $latestTicketInformation->techniqueTicket->name }}<br>
                                                     @endif
                                                 @endif
                                                 @php $color = ''; @endphp
