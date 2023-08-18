4a327b73f3ded69f4d6beb8f4e3f2a00095b111b
Antonio Tomas <antoniotd87@gmail.com>
2023-08-18 14:29:46 -0600

    Fix al mostrar los diseñadores disponibles

--- resources/views/livewire/monitor-asignacion-component.blade.php
+++ resources/views/livewire/monitor-asignacion-component.blade.php
@@ -4,7 +4,7 @@
             <div class="col-md-4 shadow">
                 <strong>{{ $designerByType['type'] }}</strong><br>
                 <br>
-                @if (array_key_exists('designers', $designerByType['designer']))
+                @if (array_key_exists('designers', $designerByType))
                     <strong>Diseñadores Contemplados</strong>
                     <br>
                     @foreach ($designerByType['designers'] as $designer)
