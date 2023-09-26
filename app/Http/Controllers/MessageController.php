7ff19a2e50da75267ce97258ed22088bb9cfa001
samantha-spindola <gisellesamspindola@gmail.com>
2023-09-26 13:45:58 -0600

    actualizaciones del problema del receriver_id terminadas

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -43,24 +43,32 @@ class MessageController extends Controller
         $transmitter_id = auth()->user()->id;
         $transmitter_name = auth()->user()->name . ' ' . auth()->user()->lastname;
         $userReceiver = '';
-        /*  if (auth()->user()->isAbleTo(['attend-ticket'])) {
+        /*     if (auth()->user()->isAbleTo(['attend-ticket'])) {
             $userReceiver = User::find($ticket->creator_id);
         } else if (auth()->user()->isAbleTo(['create-ticket'])) {
             $userReceiver = User::find($ticket->designer_id);
         } */
-        /*   $user = User::where('id', $ticket->creator_id)->first();
+        /*     $user = User::where('id', $ticket->creator_id)->first();
         dd($user); */
         if (auth()->user()->isAbleTo(['attend-ticket'])) {
             $userReceiver = User::where('id', $ticket->creator_id)->first();
         } else if (auth()->user()->isAbleTo(['create-ticket'])) {
-            $userReceiver = User::where('id', $ticket->creator_id)->first();
+            $userReceiver = User::where('id', $ticket->designer_id)->first();
+        }
+        if ($userReceiver == null) {
+            $receiver_id = null;
+            $receiver_name = null;
+        } else {
+
+            // Guardar el mensaje con los sigioetes datos
+            // Creamos el mensaje y lo guardamos en la base de datos
+
+
+            $receiver_id = $userReceiver->id;
+            $receiver_name = $userReceiver->name . ' ' . $userReceiver->lastname;
         }
 
 
-        $receiver_id = $userReceiver->id;
-        $receiver_name = $userReceiver->name . ' ' . $userReceiver->lastname;
-        // Guardar el mensaje con los sigioetes datos
-        // Creamos el mensaje y lo guardamos en la base de datos
         $message = Message::create([
             "transmitter_id" => $transmitter_id,
             "transmitter_name" => $transmitter_name,
@@ -78,11 +86,15 @@ class MessageController extends Controller
             'type' => 'message'
         ]);
         //Mensaje
+        if ($receiver_id !== null){
+
+
         try {
             event(new MessageSendEvent($message->message, $receiver_id, $transmitter_name));
             $userReceiver->notify(new MessageNotification($ticket->id, $ticket->latestTicketInformation->title, $transmitter_name, $message->message));
         } catch (Exception $th) {
         }
+     }
         // Regresar a la misma vista AtenderTicket (ticket.show)
         if (auth()->user()->isAbleTo(['attend-ticket'])) {
             return redirect()->action('DesignerController@show', ['ticket' => $ticket->id]);
